228b1cd29e47a6550ae9098222da984c13f8647d
Karthik Arkalgud Nagaraj <ankarthikengg@gmail.com>
2024-08-22 20:08:39 +0530

    Updated prod env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: "temple-accounts-556e6",
+    appId: "1:132896477598:web:41a427ff6f06c65857068f",
+    storageBucket: "temple-accounts-556e6.appspot.com",
+    apiKey: "AIzaSyDOdPtsrEafCXKOC5nwAXr9KvU7pNMxam8",
+    authDomain: "temple-accounts-556e6.firebaseapp.com",
+    messagingSenderId: "132896477598",
+    measurementId: "G-JV6LKNZ5T3"
+  },
+  appCheck: {
+    siteKey: '6Lc1UygqAAAAAMTofSdpYhD6mzw7kokYHuvZ5tCr'
+  }
 };
